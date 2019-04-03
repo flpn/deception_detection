a7f53c6861e51e6c741ce81719410d216173ea5a
Felipe Netto <flpncc@gmail.com>
2019-04-03 20:44:44 -0300

    Replacing the six categories for just two

--- deception_detection.py
+++ deception_detection.py
@@ -14,6 +14,8 @@ from keras.wrappers.scikit_learn import KerasClassifier
 
 
 def split_dataframe(dataframe):
+    dataframe['Label'] = dataframe['Label'].replace(['pants-fire', 'barely-true'], 'false').replace(['half-true', 'mostly-true'], 'true')
+    
     X = dataframe.iloc[:, 2].values
     y = dataframe.iloc[:, 1].values
     
@@ -30,7 +32,7 @@ def stemming_documents(documents):
     stemmed_documents = []
     
     for document in documents:
-        sentence = ' '.join([stemmer.stem(word) for word in whitespace_tokenizer.tokenize(document)])
+        sentence = ' '.join([stemmer.stem(word).lower() for word in whitespace_tokenizer.tokenize(document)])
         stemmed_documents.append(sentence)
     
     return np.array(stemmed_documents, dtype='object')
@@ -46,9 +48,6 @@ def preprocess_datasets(X, y, dataset_type, tf_idf):
            
     y = encode_categorical_data(y)
     y = y.reshape(-1, 1)
-    
-    one_hot_encoder = OneHotEncoder(categorical_features=[0])
-    y = one_hot_encoder.fit_transform(y).toarray()
 
     return X, y
 
@@ -59,32 +58,21 @@ def build_ann_classifier(rate=0.1, lr=0.01):
     classifier.add(Dense(units=512, kernel_initializer='uniform', activation='relu', input_shape=(10230,)))
     classifier.add(Dropout(rate=rate))
     classifier.add(Dense(units=512, kernel_initializer='uniform', activation='relu'))
+    classifier.add(Dropout(rate=rate))
     classifier.add(Dense(units=256, kernel_initializer='uniform', activation='relu'))
+    classifier.add(Dropout(rate=rate))
     classifier.add(Dense(units=128, kernel_initializer='uniform', activation='relu'))
+    classifier.add(Dropout(rate=rate))
     classifier.add(Dense(units=64, kernel_initializer='uniform', activation='relu'))
-    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='softmax'))
+    classifier.add(Dense(units=32, kernel_initializer='uniform', activation='relu'))
+    classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
 
-    adam = optimizers.Adam(lr=lr)
-    
-    classifier.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+#    adam = optimizers.Adam(lr=lr)
+#    classifier.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
 
-    return classifier
+    rmsprop = optimizers.RMSprop(lr=lr)
+    classifier.compile(optimizer=rmsprop, loss='binary_crossentropy', metrics=['accuracy'])
 
-
-def build_cnn_classifier(lr=0.001):
-    classifier = Sequential()
-    
-    classifier.add(Conv1D(64, 2, input_shape=(10230, 1), activation='relu'))
-    classifier.add(MaxPooling1D(pool_size=4))
-    classifier.add(Conv1D(32, 5, activation='relu'))
-    classifier.add(MaxPooling1D(pool_size=2))
-    classifier.add(Flatten())
-    classifier.add(Dense(units=128, activation='relu'))
-    classifier.add(Dense(units=6, activation='softmax'))
-    
-    adam = optimizers.Adam(lr=lr)
-    classifier.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
-    
     return classifier
 
 
@@ -119,17 +107,28 @@ dataset = concat_datasets(pd.read_csv('train.tsv', sep='\t'),
                           pd.read_csv('valid.tsv', sep='\t'))
 X, y = split_dataframe(dataset)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
-tf_idf = TfidfVectorizer(max_df=0.5)
+tf_idf = TfidfVectorizer(max_df=0.5, ngram_range=(2, 2))
 
 X_train, y_train = preprocess_datasets(X_train, y_train, 'train', tf_idf)
 X_test, y_test = preprocess_datasets(X_test, y_test, 'test', tf_idf)
 
 # uncomment for simple trainning
-#classifier = build_ann_classifier()
-#classifier.fit(X_train, y_train, batch_size=100, epochs=5, shuffle=True, validation_split=0.25)
+classifier = build_ann_classifier()
+classifier.fit(X_train, y_train, batch_size=50, epochs=2, shuffle=True, validation_split=0.25)
 
-# uncomment for cv on cnn
-#X_train = np.expand_dims(X_train, axis=2)
-accuracy, variance = k_fold_cross_validation(X_train, y_train, build_ann_classifier, 5, 50, 3)
+# uncomment for CV
+# accuracy, variance = k_fold_cross_validation(X_train, y_train, build_ann_classifier, 5, 50, 2)
+# best acc = 0.6125530848050055, var = 0.0033601900549687412 rmsprop
 
-# best ann: 3 epochs, 50 batch, 0.01 lr, 512, 512, 256, 128, 64, 6 ->  a, v = 0.2343870439336436, 0.009490015866157608
+# uncomment to predict a unique news
+# news = np.array(['Our debt to GDP ratios, our deficit to GDP ratios are quickly approaching the countries we have been reading about for the last year and a half. We are not far behind Ireland, Greece, Portugal and Spain and all these countries we have been reading about.'])
+# news = tf_idf.transform(news).toarray()
+# response = classifier.predict(news) 
+
+# uncomment for parameter tuning
+#parameters = {'batch_size': [32, 50, 128, 256],
+#              'epochs': [2, 3],
+#              'lr': [0.001, 0.01, 0.1]}
+#grid_search = find_optimal_hyperparameters(X_train, y_train, 5, parameters)
+#best_parameters = grid_search.best_params_
+#best_accuracy = grid_search.best_score_
